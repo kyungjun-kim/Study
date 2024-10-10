72c20414af785ffec3267ca6e6ce75506b0d4e99
KyungJun_Kim <kkj214@gmail.com>
2024-10-10 13:53:57 +0900

    commit

--- Django_Study_Programmers/mysite/polls/models.py
+++ Django_Study_Programmers/mysite/polls/models.py
@@ -8,4 +8,10 @@ from django.db import models
 # 질문 : 여름에 놀러간다면 어디에 갈래?
 
 class Question(models.Model) :
-    question = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("Date published")
+
+class Choice(models.Model) :
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
