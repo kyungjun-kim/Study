ffea2509607daac2fdbc292113a7c8f14ebbfa50
KyungJun_Kim <kkj214@gmail.com>
2024-10-12 16:08:26 +0900

    Django_Study
    Django_Study 갱신

--- Django_Study_Programmers/mysite/polls/models.py
+++ Django_Study_Programmers/mysite/polls/models.py
@@ -9,9 +9,18 @@ from django.db import models
 
 class Question(models.Model) :
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("Date published")
+
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'제목 : {self.question_text}, 날짜 : {self.pub_date}'
 
 class Choice(models.Model) :
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
     choice_text = models.CharField(max_length=200)
+
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.choice_text}"
