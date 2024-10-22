3d0d2f8f1a27d63ac93ee4513b1d36b2c27b7350
KyungJun_Kim <kkj214@gmail.com>
2024-10-22 22:23:00 +0900

    장고 강의 수강 및 코드 작성

--- Django_Study_Programmers/mysite/polls/urls.py
+++ Django_Study_Programmers/mysite/polls/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "polls "
 urlpatterns = [
-    path('',views.index, name='index'),
-    path("some_url", views.some_url),
+    path('', views.index, name='index'),
+    path('some_url', views.some_url),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/result/', views.vote, name='result'),
 ]
--- Django_Study_Programmers/mysite/polls/views.py
+++ Django_Study_Programmers/mysite/polls/views.py
@@ -1,10 +1,48 @@
 #from django.shortcuts import render
 
-from django.http import HttpResponse
-# Create your views here.
+from .models import *
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.db.models import F
 
-def index(requests):
-    return HttpResponse("Hello, World!")
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"questions": latest_question_list}
+    return render(request, "polls/index.html", context)
 
-def some_url(requests):
-    return HttpResponse("Some_URL 구현")
+def detail(request, question_id):
+    """try :
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")"""
+    
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question" : question})
+
+def some_url(request) :
+    return HttpResponse("Some Url을 구현")
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': f"선택이 없습니다. id={request.POST['choice']}"})
+
+    else:
+        # A서버에서도 Votes = 0
+        # B서버에서도 Votes = 0
+        
+        #selected_choice.votes += 1
+        #투표 수 +1
+        selected_choice.votes = F('votes') + 1
+
+        selected_choice.save()
+        #투표 수 저장
+        
+        return HttpResponseRedirect(reverse('polls:result', args = (question_id,)))
+    
+def result(request, question_id) :
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {'question' : question})
